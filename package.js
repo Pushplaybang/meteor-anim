ca3b392c062d6eb9a52e9aad97aa39ca0d36e082
Paul van Zyl <paul@nonacreative.com>
2016-04-05 08:43:42 +0200

    Versioning deps

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('velocityjs:velocityjs');
+  api.use('velocityjs:velocityjs@1.2.1');
   api.addFiles('anim.js', 'client');
   api.export('Anim', 'client');
 });
